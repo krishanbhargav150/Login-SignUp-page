673c929f38db5450dd52ffb262d9608737394502
Krishan <krishan.bhargav@talentica.com>
2026-07-08 22:06:49 +0530

    practise redux toolkit

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import AuthForm from './AuthForm';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './features/counter/counterSlice.ts'
+
 
 function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
   return (
     <div>
+      <h1>Login Form</h1>
+      <h2>Counter: {count}</h2>
+      <button onClick = {() => dispatch(increment())}>Increase</button>
+      <button onClick = {() => dispatch(decrement())}>Decrease</button>
       <AuthForm />
+
     </div>
   );
 }
